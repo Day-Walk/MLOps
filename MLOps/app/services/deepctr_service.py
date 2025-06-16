5b6a124681dcebfc3b9d06d2fd153da91ef97338
skqorrla <beomseo00@gmail.com>
2025-06-16 16:37:29 +0900

    fix: DeepCTRService 및 UserDataService의 데이터 경로를 컨테이너 내 경로로 수정 및 docker-compose.yml에 데이터 볼륨 추가

--- MLOps/app/services/deepctr_service.py
+++ MLOps/app/services/deepctr_service.py
@@ -9,7 +9,7 @@ class DeepCTRService:
     """DeepCTR 모델 서비스"""
     
     def __init__(self):
-        self.model = DeepFMModdelTrain("~/MLOps/data/final_click_log.csv")
+        self.model = DeepFMModdelTrain("/app/data/final_click_log.csv")
         self.elk_client = ELKClient()
         self.user_data_service = UserDataService()
         
